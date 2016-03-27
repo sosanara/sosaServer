1b55402b594725bd41dc46ba6d3dc14c07acae75
jicjjang <jicjjang12@gmail.com>
2016-03-27 19:49:04 +0900

    email setting value

---
+++ conf/ex.email.py
@@ -0,0 +1,19 @@
+
+def site_id():
+    return 1
+
+
+def host():
+    return 'smtp.domain.com'
+
+
+def host_user():
+    return 'mail_id@domain.com'
+
+
+def host_password():
+    return 'password'
+
+
+def from_email():
+    return host_user()
