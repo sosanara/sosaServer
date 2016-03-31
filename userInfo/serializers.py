f7760cd2632b6aae8ac8322f59bd20ac9cbd8fd0
jicjjang <jicjjang12@gmail.com>
2016-03-31 18:20:34 +0900

    add userInfo, history_list, history_detail

--- userInfo/serializers.py
+++ userInfo/serializers.py
@@ -1,10 +1,77 @@
+# -*- coding: utf-8 -*-
 
-from models import MyUser
 from rest_framework import serializers
 
+from .models import MyUser
 
-class MyUserSerializer(serializers.ModelSerializer):
+from django.utils.translation import ugettext_lazy as _
+
+
+def _validate_user(user1, user2):
+    if user1.id != user2.id:
+        raise serializers.ValidationError({"user": _("You are not have authorization.")})
+
+
+def _validate_image(my_picture):
+    if my_picture.image is None:
+        raise serializers.ValidationError({"image": _("Image was not found.")})
+
+
+class MyUserDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = MyUser
         fields = ('username', 'first_name', 'last_name', 'birth', 'gender', 'email')
+
+
+class MyStaticDetailSerializer(serializers.Serializer):
+    def _validate_user(self, all_users):
+        if all_users is None:
+            raise serializers.ValidationError({"all_users": _("This image is not yours. Please check your image.")})
+        # return all_users.
+
+
+    def _validate_image(self, kwargs):
+        my_picture = MyPicture.objects.filter(id=kwargs['picture_id']).first()
+        if my_picture is None:
+            raise serializers.ValidationError({"image": _("Image was not found.")})
+        return {
+            'picture': my_picture
+        }
+
+    def get_cleaned_data(self, user, all_users):
+        self.cleaned_data = self._validate_user(all_users)
+        self._validate_user(user, self.cleaned_data['picture'].user)
+        return self.cleaned_data['picture']
+
+
+class MyHistoryListSerializer(serializers.Serializer):
+
+    def get_cleaned_data(self, user, my_pictures):
+        pictures = {}
+        for my_picture in my_pictures:
+            _validate_user(user, my_picture.user)
+            _validate_image(my_picture)
+            pictures[my_picture.id] = 'uploads/' + my_picture.image.name
+        return pictures
+
+
+class MyHistoryDetailSerializer(serializers.Serializer):
+
+    def get_cleaned_data(self, user, my_picture):
+        _validate_user(user, my_picture.user)
+        _validate_image(my_picture)
+        self.cleaned_data = {
+            'name': my_picture.image.name,
+            'created_date': my_picture.created_date,
+            'type': my_picture.result.type,
+        }
+        return self.cleaned_data
+
+
+class MyGraphListSerializer(serializers.Serializer):
+    pass
+
+
+class MyGraphDetailSerializer(serializers.Serializer):
+    pass
--- userInfo/views.py
+++ userInfo/views.py
@@ -1,33 +1,147 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
 
-from rest_framework.generics import RetrieveAPIView
+#  Create your views here.
+from pprint import pprint
+from datetime import datetime
+
+from rest_framework.generics import RetrieveAPIView, ListAPIView
+from rest_framework.mixins import DestroyModelMixin, RetrieveModelMixin
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_200_OK
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import exceptions
 
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from django.utils.translation import ugettext_lazy as _
 
-from userInfo.models import MyUser
-from userInfo.serializers import MyUserSerializer
+from picture.models import MyPicture
+from .models import MyUser
+from .serializers import MyUserDetailSerializer,\
+    MyStaticDetailSerializer,\
+    MyGraphListSerializer,\
+    MyGraphDetailSerializer,\
+    MyHistoryListSerializer,\
+    MyHistoryDetailSerializer
 
 
-class TempMixin(object):
-    queryset = MyUser.objects.all()
-    serializer_class = MyUserSerializer
+def response(value, message, status=HTTP_200_OK):
+    return Response({
+            "success": _(message),
+            "value": value
+        }, status=status)
 
 
-class UserDetail(TempMixin, RetrieveAPIView):
-    serializer_class = MyUserSerializer
+class UserDetail(RetrieveAPIView):
+    serializer_class = MyUserDetailSerializer
+    permission_classes = (IsAuthenticated,)
+    allowed_methods = ('GET', 'OPTIONS', 'HEAD')
 
     def get_object(self):
         return self.request.user
 
 
-class StatisticDetail(TempMixin, RetrieveAPIView):
-    pass
+class StatisticDetail(RetrieveAPIView):
+    serializer_class = MyStaticDetailSerializer
+    permission_classes = (IsAuthenticated,)
+    allowed_methods = ('GET', 'OPTIONS', 'HEAD')
+
+    def get_queryset(self):
+        return MyPicture.objects.all().exclude(id=self.request.user_id)
+
+    def get_my_age(self, request):
+        pprint(datetime.today().year - request.user.birth)
+        return datetime.today().year - request.user.birth
+
+    def get_same_ages(self, my_age):
+        filtered_picture = []
+        for picture in self.get_queryset():
+            if (picture.created_date.year - picture.user.birth)-5 < my_age or \
+                                    (picture.created_date.year - picture.user.birth)+5 > my_age:
+                filtered_picture.append(picture)
+
+        return filtered_picture
+
+    def retrieve(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        my_age = self.get_my_age(request)
+        same_ages = self.get_same_ages(my_age)
+        statistic = serializer.get_cleaned_data(request.user, self.get_queryset())
+        return response(statistic, "It was successful statistic detail search.")
 
 
-class GraphViewSet(TempMixin, ReadOnlyModelViewSet):
-    pass
+class HistoryList(ListAPIView):
+    serializer_class = MyHistoryListSerializer
+    permission_classes = (IsAuthenticated,)
+    allowed_methods = ('GET', 'OPTIONS', 'HEAD')
+
+    def get_queryset(self):
+        try:
+            return MyPicture.objects.filter(user=self.request.user)
+        except MyPicture.DoesNotExist:
+            raise exceptions.NotFound()
+
+    def list(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        history_list = serializer.get_cleaned_data(request.user, self.get_queryset())
+        return response(history_list, "It was successful history list search.")
 
 
-class HistoryViewSet(TempMixin, ModelViewSet):
-    pass
+class HistoryDetail(RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
+    serializer_class = MyHistoryDetailSerializer
+    permission_classes = (IsAuthenticated,)
+    allowed_methods = ('GET', 'DELETE', 'OPTIONS', 'HEAD')
+
+    def get_queryset(self):
+        try:
+            return MyPicture.objects.get(id=self.kwargs['history_id'])
+        except MyPicture.DoesNotExist:
+            raise exceptions.NotFound()
+
+    def retrieve(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        history_detail = serializer.get_cleaned_data(request.user, self.get_queryset())
+        return response(history_detail, "It was successful history detail search.")
+
+    def destroy(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        instance = self.get_queryset()
+        history_detail = serializer.get_cleaned_data(request.user, instance)
+        self.perform_destroy(instance)
+        return response(history_detail, "Successful removed History Parts.", status=HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        instance.delete()
+
+
+class GraphList(ListAPIView):
+    serializer_class = MyGraphListSerializer
+    permission_classes = (IsAuthenticated,)
+    allowed_methods = ('GET', 'OPTIONS', 'HEAD')
+
+    def get_queryset(self):
+        try:
+            return MyUser.objects.get(id=self.request.user.id)
+        except MyUser.DoesNotExist:
+            raise exceptions.NotFound()
+
+    def list(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        graph_list = serializer.get_cleaned_data(request.user, kwargs)
+        return response(graph_list, "It was successful graph list search.")
+
+
+class GraphDetail(RetrieveAPIView):
+    serializer_class = MyGraphDetailSerializer
+    permission_classes = (IsAuthenticated,)
+    allowed_methods = ('GET', 'OPTIONS', 'HEAD')
+
+    def get_queryset(self):
+        try:
+            return MyUser.objects.get(id=self.request.user.id)
+        except MyUser.DoesNotExist:
+            raise exceptions.NotFound()
+
+    def retrieve(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        graph_detail = serializer.get_cleaned_data(request.user, kwargs)
+        return response(graph_detail, "It was successful graph detail search.")
