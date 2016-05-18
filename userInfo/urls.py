2a5766a0aaf9526031b2d140f763a7f7955c515b
jicjjang <jicjjang12@gmail.com>
2016-05-18 10:31:58 +0900

    change wording. Graph->History, History->Gallery

--- userInfo/urls.py
+++ userInfo/urls.py
@@ -1,18 +1,17 @@
 from django.conf.urls import url, include
 
 from sosaServer.viewset import (
-    statistic_list, statistic_detail, graph_list, graph_detail, history_list, history_detail
+    statistic_list, gallery_list, gallery_detail, history_list, history_detail
 )
 
 
 urlpatterns = [
     url(r'statistic/', include([
         url(r'^$', statistic_list, name="statistic_list"),
-        url(r'^(?P<statistic_id>\d+)/$', statistic_detail, name="statistic_detail"),
     ])),
-    url(r'^graph/', include([
-        url(r'^$', graph_list, name="graph_list"),
-        url(r'^(?P<graph_id>\d+)/$', graph_detail, name="graph_detail"),
+    url(r'^gallery/', include([
+        url(r'^$', gallery_list, name="gallery_list"),
+        url(r'^(?P<gallery_id>\d+)/$', gallery_detail, name="gallery_detail"),
     ])),
     url(r'^history/', include([
         url(r'^$', history_list, name="history_list"),
