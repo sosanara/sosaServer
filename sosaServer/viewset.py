73c6b8a8c76fa19d68c8e190283f2dec030a7508
jicjjang <jicjjang12@gmail.com>
2016-05-18 10:33:42 +0900

    change wording and remove statistic_detail

--- sosaServer/viewset.py
+++ sosaServer/viewset.py
@@ -1,24 +1,24 @@
 from picture.views import (PictureList, PictureDetail)
 from userInfo.views import (
-    StatisticList, StatisticDetail, HistoryList, HistoryDetail, GraphList, GraphDetail
+    StatisticList, UserDetail, GalleryList, GalleryDetail, HistoryList, HistoryDetail
 )
 
 
-statistic_list = StatisticList.as_view()
+user_detail = UserDetail.as_view()
 
-statistic_detail = StatisticDetail.as_view()
+statistic_list = StatisticList.as_view()
 
 picture_list = PictureList.as_view()
 
 picture_detail = PictureDetail.as_view()
 
-history_list = HistoryList.as_view()
+gallery_list = GalleryList.as_view()
 
-history_detail = HistoryDetail.as_view({
+gallery_detail = GalleryDetail.as_view({
     'get': 'retrieve',
     'delete': 'destroy'
 })
 
-graph_list = GraphList.as_view()
+history_list = HistoryList.as_view()
 
-graph_detail = GraphDetail.as_view()
+history_detail = HistoryDetail.as_view()
